b460d3537bb25949c3f05be5c2b37bc82c720e37
SankalanPalChowdhury <37496194+SankalanPalChowdhury@users.noreply.github.com>
2018-10-24 22:51:04 +0530

    Update jack_bot.py

--- jack_bot.py
+++ jack_bot.py
@@ -8,6 +8,7 @@ dlr_probs = probs.set_dealer(prob_face)
 #use -d flag for debug
 if (sys.argv[-1]=="-d"):
 	print ".\t2 3 4 5 6 7 8 9 F A"
+probs.refresh()
 probs.get_hard(prob_face, dlr_probs)
 probs.get_soft(prob_face, dlr_probs)
 probs.get_pair(prob_face, dlr_probs)
